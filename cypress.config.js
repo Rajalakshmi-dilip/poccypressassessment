69b0262c464dca36bf4938c65916fa897b7e9ac9
Rajalakshmi-dilip <rajalakshmi.d@optisolbusiness.com>
2025-04-16 19:06:17 +0530

    merged Html report

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,19 @@
 const { defineConfig } = require('cypress');
+const mochawesome = require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
-  experimentalStudio: true,
-  reporter: 'mochawesome',
+  reporter: 'cypress-multi-reporters',
   reporterOptions: {
-    reportDir: 'report',
-    overwrite: false,
-    html: true,
-    json: true
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    cypressMochawesomeReporterReporterOptions: {
+      reportDir: 'report',
+      overwrite: false,
+      html: false,
+      json: true
+    },
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'report/junit/test-results.[hash].xml'
+    }
   },
   retries: {
     runMode: 2,
@@ -15,7 +21,10 @@ module.exports = defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+      // Initialize cypress-mochawesome-reporter plugin
+      mochawesome(on);
+      return config;
+    }
+  }
 });
+
